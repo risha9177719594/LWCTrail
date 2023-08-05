c1fe4af4ddd4865c7e0ca1723664a88543fe1225
rajendar bommidi <risha.9177719594@gmail.com>
2023-08-05 17:07:55 +0530

    Update server.js

--- scripts/server.js
+++ scripts/server.js
@@ -3,7 +3,7 @@ const compression = require('compression');
 const helmet = require('helmet');
 const express = require('express');
 const path = require('path');
-
+ 
 const app = express();
 app.use(helmet());
 app.use(compression());
